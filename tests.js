1e4fe70ec287f94a750cf1364d45cee1cde8fc2f
sashajustice <sabrinnasreddin@gmail.com>
2017-01-31 08:55:53 -0800

    added test and answer folders

--- tests.js
+++ tests.js
@@ -4,6 +4,26 @@
 
 import chai, { expect } from 'chai'
 import chaiChange from 'chai-change'
-import PriorityQueue from '../src/PriorityQueue'
+import Compuguess from '../src/Compuguess'
 
 chai.use(chaiChange)
+
+/*1: Guess The Number
+Write a program that will generate a number between 1 and 100. Your program will ask the user to guess the generated number.When the user types a guess, the program should tell them if they won (the guess matched the computer's number), if their guess was higher than the number the computer generated, OR if the guess was lower than the number the computer generated. Terminate the program if the user types "exit". */
+
+
+describe('compuGuess()', () => {
+  it('should not generate a number above 100', () => {
+    console.log('Guess the generated number');
+    expect()).to.equal('1-100')
+
+  it('should tell them if they won')
+  })
+})
+it("should tell them if ")
+
+describe('compuGuess()', () => {
+  it('should not add two numbers', () => {
+    expect(concat(1,2)).to.equal(null)
+  })
+})
