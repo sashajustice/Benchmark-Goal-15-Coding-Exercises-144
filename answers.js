a635f93329be4ba7f35bfca28c7a34a5c0c35d18
sashajustice <sabrinnasreddin@gmail.com>
2017-01-30 16:26:58 -0800

    added test file and answer file

---
+++ answers.js
@@ -0,0 +1,8 @@
+Specifications
+1: Guess The Number
+
+Write a program that will generate a number between 1 and 100. Your program will
+ ask the user to guess the generated number.
+
+When the user types a guess, the program should tell them if they won (the guess
+   matched the computer's number), if their guess was higher than the number the computer generated, OR if the guess was lower than the number the computer generated. Terminate the program if the user types "exit".
---
+++ tests.js
@@ -0,0 +1,9 @@
+
+'use strict'
+
+
+import chai, { expect } from 'chai'
+import chaiChange from 'chai-change'
+import PriorityQueue from '../src/PriorityQueue'
+
+chai.use(chaiChange)
